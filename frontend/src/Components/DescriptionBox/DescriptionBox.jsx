9983933d899f8855c2b69863b725e06e8e8a193a
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-06-05 12:28:40 +0700

    Add frontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quo nesciunt cumque laudantium itaque fugit cum fuga esse id temporibus? Laudantium ad itaque esse impedit quibusdam saepe natus, dolor tempore.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate voluptates nemo recusandae dolores voluptatum adipisci culpa perspiciatis nulla ullam deleniti quaerat, magni quasi eum hic animi fugit obcaecati facere!</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
