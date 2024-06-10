3117be348e1fe0d5a74f452bbd67a6c71562f5b3
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-06-10 20:16:14 +0700

    UI, export excel update

--- admin/src/Components/Discount/Discount.jsx
+++ admin/src/Components/Discount/Discount.jsx
@@ -1,5 +1,6 @@
 import {React,useEffect,useState} from 'react'
 import './Discount.css'
+import * as XLSX from 'xlsx';
 
 const Discount = () => {
     const [discountdetails, setDiscountdetails] = useState({code:'',type:'',date:''})
@@ -40,53 +41,65 @@ const Discount = () => {
         .then((res) => res.json())
         .then((data) => {setDiscounts(data)})
     }
-    useEffect(() =>{fetchInfo()}, [])
-  return (
-    <div className='add-and-list_code'>
-        <div className="add-code">
-            <div className="addcode-itemfield">
-                <p>Code</p>
-                <input value={discountdetails.code} onChange={changeHandler} type="text" name='code' placeholder='Type here'/>
-            </div>
-            <div className="addcode-itemfield">
-                <p>Code type</p>
-                <select value={discountdetails.type} onChange={changeHandler} name="type" className='add-code-selector'>
-                    <option value="" hidden disabled selected>Select type</option>
-                    <option value="delivery">Delivery</option>
-                    <option value="product">Product</option>
-                </select>
-            </div>
-            <div className="addcode-itemfield">
-                <p>Date expired</p>
-                <input value={discountdetails.date} onChange={changeHandler} type="date" name='date'/>
-            </div>            
-            <button onClick={Add_Discount} className='addcode-btn'>Add</button>
-        </div>
-
-        <div className="list-code">
-            <h1>All Discount List</h1>
-            <div className="listcode-format-main">
-                <p>Code</p>
-                <p>Type of code</p>
-                <p>Date</p>
-            </div>
-            <div className="listcode-allcodes">
-                <hr />
-                {discounts.map((discount,index) => {
-                    return <>
-                        <div key={index} className='listcode-format-main listcode-format'>
-                        <p>{discount.code}</p>
-                        <p>{discount.type}</p>
-                        <p>{discount.date}</p>
-                   </div>
-                    <hr />
-                    </>
-                })}
-            </div>
-        </div>
+    useEffect(() => {fetchInfo()}, [])
     
-    </div>
-  )
+    
+    const exportToExcel = () => {
+        const discountForExcel = discounts.map(({ code, type, date}) => ({code, type, date}));
+    
+        const ws = XLSX.utils.json_to_sheet(discountForExcel);
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Discounts");
+        XLSX.writeFile(wb, "discounts.xlsx");
+    }
+
+    return (
+        <div className='add-and-list_code'>
+            <div className="add-code">
+                <div className="addcode-itemfield">
+                    <p>Code</p>
+                    <input value={discountdetails.code} onChange={changeHandler} type="text" name='code' placeholder='Type here'/>
+                </div>
+                <div className="addcode-itemfield">
+                    <p>Code type</p>
+                    <select value={discountdetails.type} onChange={changeHandler} name="type" className='add-code-selector'>
+                        <option value="" hidden disabled selected>Select type</option>
+                        <option value="delivery">Delivery</option>
+                        <option value="product">Product</option>
+                    </select>
+                </div>
+                <div className="addcode-itemfield">
+                    <p>Date expired</p>
+                    <input value={discountdetails.date} onChange={changeHandler} type="date" name='date'/>
+                </div>            
+                <button onClick={Add_Discount} className='addcode-btn'>Add</button>
+            </div>
+
+            <div className="list-code">
+            <button className='export-excel' onClick={exportToExcel}>Export to Excel</button>
+                <h1>All Discount List</h1>
+                <div className="listcode-format-main">
+                    <p>Code</p>
+                    <p>Type of code</p>
+                    <p>Date</p>
+                </div>
+                <div className="listcode-allcodes">
+                    <hr />
+                    {discounts.map((discount,index) => {
+                        return <>
+                            <div key={index} className='listcode-format-main listcode-format'>
+                            <p>{discount.code}</p>
+                            <p>{discount.type}</p>
+                            <p>{discount.date}</p>
+                    </div>
+                        <hr />
+                        </>
+                    })}
+                </div>
+            </div>
+        
+        </div>
+    )
 }
 
 export default Discount
--- admin/src/Components/Payment/Payment.jsx
+++ admin/src/Components/Payment/Payment.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Payment.css'
+import * as XLSX from 'xlsx';
 
 const Payment = () => {
     const [allpayments, setAllPayments] = useState([])
@@ -11,33 +12,44 @@ const Payment = () => {
     useEffect(() => {
         fetchInfo()
     }, [])
-  return (
-    <div className="list-payment">
-        <h1>All Payments List</h1>
-        <div className="listpayment-format-main">
-            <p>Order Id</p>
-            <p>User Email</p>
-            <p>Total Fee</p>
-            <p>Transaction Status</p>
-            <p>Payment Date</p>
+
+    const exportToExcel = () => {
+        const paymentsForExcel = allpayments.map(({ orderId, user_email,amount, date,status}) => ({orderId, user_email,amount, date,status}));
+    
+        const ws = XLSX.utils.json_to_sheet(paymentsForExcel);
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Payments");
+        XLSX.writeFile(wb, "payments.xlsx");
+    }
+
+    return (
+        <div className="list-payment">
+            <button className='export-excel' onClick={exportToExcel}>Export to Excel</button>
+            <h1>All Payments List</h1>
+            <div className="listpayment-format-main">
+                <p>Order Id</p>
+                <p>User Email</p>
+                <p>Total Fee</p>
+                <p>Transaction Status</p>
+                <p>Payment Date</p>
+            </div>
+            <div className="listpayment-allpayments">
+                <hr />
+                {allpayments.map((payment,index)=>{
+                    return <>
+                        <div key={index} className='listpayment-format-main listpayment-format'>
+                            <p>{payment.orderId}</p>
+                            <p>{payment.user_email}</p>
+                            <p>{payment.amount}</p>
+                            <p>{payment.status}</p>
+                            <p>{payment.date}</p>
+                        </div>
+                        <hr />
+                    </>
+                })}
+            </div>
         </div>
-        <div className="listpayment-allpayments">
-            <hr />
-            {allpayments.map((payment,index)=>{
-                return <>
-                    <div key={index} className='listpayment-format-main listpayment-format'>
-                        <p>{payment.orderId}</p>
-                        <p>{payment.user_email}</p>
-                        <p>{payment.amount}</p>
-                        <p>{payment.status}</p>
-                        <p>{payment.date}</p>
-                    </div>
-                    <hr />
-                </>
-            })}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Payment
